35730249b08eae6bb4f2c60adcc1e5fe432785d2
Dennis <dennis.g.tam@gmail.com>
2015-03-22 16:55:24 -0500

    Commited Dennis's changes

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  #Sets the value of the matrix
+  setMatrix <- function(xPrime){
+    x <<- xPrime
+    inverse <<- NULL
+  }
+  
+  #Returns the matrix
+  getMatrix <- function(){
+    x
+  }
+  
+  #Sets the inverse of the matrix
+  setInverse <- function(theInverse){
+    inverse <<- theInverse
+  }
+  
+  #Returns the inverse of the matrix
+  getInverse <- function(){
+    inverse
+  }
+  
+  #A "cached" matrix with its inverse
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +36,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  #Gets the inverse of the matrix in the "cached" matrix list
+  theInverse <- x$getInverse()
+  
+  #If theInverse is NOT NULL, then the inverse has been cached and returns the cached inverse
+  if(!is.null(theInverse)){
+    message("Getting cached inverse")
+    return(theInverse)
+  }
+  
+  #Get the matrix in the list
+  theMatrix <- x$getMatrix()
+  #Get the inverse of the matrix and assign it to theInverse
+  message("Solving for inverse")
+  theInverse <- solve(theMatrix)
+  #sets the inverse in the list to theInverse
+  x$setInverse(theInverse)
+  #return the freshly calculated inverse
+  theInverse
 }
